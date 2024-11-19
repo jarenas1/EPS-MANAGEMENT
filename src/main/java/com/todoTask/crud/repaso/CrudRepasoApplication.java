6ee5308a7eb6ec8c33fc3d48587590df65ab7b3d
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-11-18 19:46:52 -0500

    initial commit

---
+++ src/main/java/com/todoTask/crud/repaso/CrudRepasoApplication.java
@@ -0,0 +1,13 @@
+package com.todoTask.crud.repaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRepasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRepasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todoTask/crud/repaso/CrudRepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoTask.crud.repaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
