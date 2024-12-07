17cb11d08f3e02cc93d2d5111397d73b9792d3b0
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-12-07 14:58:02 -0500

    create: ShiftEntity and ENUM with days of week

---
+++ src/main/java/com/todoTask/crud/repaso/entities/ShiftEntity.java
@@ -0,0 +1,21 @@
+package com.todoTask.crud.repaso.entities;
+
+import com.todoTask.crud.repaso.tools.enums.Day;
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+public class ShiftEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Day day;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    private Boolean active = true;
+}
---
+++ src/main/java/com/todoTask/crud/repaso/tools/enums/Day.java
@@ -0,0 +1,5 @@
+package com.todoTask.crud.repaso.tools.enums;
+
+public enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
