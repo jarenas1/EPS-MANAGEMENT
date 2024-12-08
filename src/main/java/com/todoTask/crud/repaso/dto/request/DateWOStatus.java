66052b0ff454e54e0ee5230dae368ed52e33cf95
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-12-08 18:52:26 -0500

    create: dto req wo duration and wo status for dates

--- src/main/java/com/todoTask/crud/repaso/dto/request/DateWOStatus.java
+++ src/main/java/com/todoTask/crud/repaso/dto/request/DateWOStatus.java
@@ -1,4 +1,74 @@
 package com.todoTask.crud.repaso.dto.request;
 
+import com.todoTask.crud.repaso.entities.DoctorEntity;
+import com.todoTask.crud.repaso.entities.PatientEntity;
+import com.todoTask.crud.repaso.tools.enums.DateStatus;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.time.LocalDateTime;
+//WO STATUS AND WO DURATION
 public class DateWOStatus {
+    private LocalDateTime dateTime;
+
+    private String reason;
+
+    private String notes;
+
+    private PatientEntity patient;
+
+    private DoctorEntity doctor;
+
+    public DateWOStatus(LocalDateTime dateTime, DoctorEntity doctor, String notes, PatientEntity patient, String reason) {
+        this.dateTime = dateTime;
+        this.doctor = doctor;
+        this.notes = notes;
+        this.patient = patient;
+        this.reason = reason;
+    }
+
+    public DateWOStatus() {
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public DoctorEntity getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(DoctorEntity doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public PatientEntity getPatient() {
+        return patient;
+    }
+
+    public void setPatient(PatientEntity patient) {
+        this.patient = patient;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 }
