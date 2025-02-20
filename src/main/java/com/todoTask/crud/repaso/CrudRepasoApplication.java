44e23ef762fbae090b03158ee5ae619e362cdca9
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2025-02-20 17:40:10 -0500

    create: schedule task to delete every shift every sunday at 22:00

--- src/main/java/com/todoTask/crud/repaso/CrudRepasoApplication.java
+++ src/main/java/com/todoTask/crud/repaso/CrudRepasoApplication.java
@@ -2,8 +2,10 @@ package com.todoTask.crud.repaso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CrudRepasoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/todoTask/crud/repaso/config/schedulers/ShiftShedulerConfig.java
@@ -0,0 +1,43 @@
+package com.todoTask.crud.repaso.config.schedulers;
+
+import com.todoTask.crud.repaso.repositories.ShiftRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+@EnableScheduling
+public class ShiftShedulerConfig {
+
+    Logger log = LoggerFactory.getLogger(ShiftShedulerConfig.class);
+
+    @Autowired
+    private ShiftRepository shiftRepository;
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        return scheduler;
+    }
+
+    @Scheduled(cron = "0 0 22 * * SUN") //Execute the task in SUNDAY / at 0 seg / 0 min 22 hours / * every day of month / * every month pf year
+
+    public void limpiarTurnos() {
+        try {
+            log.info("Cleaning shifts registrations...");
+
+            shiftRepository.deleteAll();
+
+            log.info("Shifts already cleaned.");
+        } catch (Exception e) {
+            log.error("Error cleaning shifts: " + e.getMessage());
+        }
+    }
+}
